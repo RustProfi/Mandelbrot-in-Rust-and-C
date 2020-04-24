9c397c1fa50bcdce4465247595a9966c7b06a902
Marno Janetzky <janetzkymarno@gmail.com>
2020-04-24 16:43:48 +0200

    show min of functions

--- python/plotter.py
+++ python/plotter.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import sys
 import numpy as np
 
-if not sys.argv:
+if len(sys.argv) == 1:
     print("usage: plotter.py <filetocresults> <filetorustresults> <title>")
     exit(1)
 
@@ -26,6 +26,19 @@ for line in lines2:
     rustresultsx.append(int(x[0]))
     rustresultsy.append(float(x[1]))
 
+#points for annotation
+ymin1 = min(cresultsy)
+xpos1 = cresultsy.index(ymin1)
+xmin1 = cresultsx[xpos1]
+
+ymin2 = min(rustresultsy)
+xpos2 = rustresultsy.index(ymin2)
+xmin2 = rustresultsx[xpos2]
+
+bbox = dict(boxstyle="round", fc="0.8")
+arrowprops = dict(
+    arrowstyle = "->",
+    connectionstyle = "angle,angleA=0,angleB=90,rad=10")
 
 plt.plot(cresultsx, cresultsy, label='C')
 plt.plot(rustresultsx, rustresultsy, label='Rust')
@@ -33,4 +46,6 @@ plt.legend()
 plt.title(sys.argv[3])
 plt.xlabel('Threads')
 plt.ylabel('Time(ms)')
+plt.annotate('Min C: %d, %.1f'%(xmin1, ymin1), (xmin1, ymin1), xytext=(xmin1, ymin1 + 1000), bbox=bbox, arrowprops=arrowprops)
+plt.annotate('Min Rust: %d, %.1f'%(xmin2, ymin2), (xmin2, ymin2), xytext=(xmin2, ymin2 + 500), bbox=bbox, arrowprops=arrowprops)
 plt.show()
